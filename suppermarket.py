0671fddf47438882ce7eca67a3ba29f5841751eb
Komron <komronabdujabbor@gmail.com>
2023-01-30 17:05:05 +0500

    Savatga tovar qoshadigan metod yaratdim

--- suppermarket.py
+++ suppermarket.py
@@ -4,6 +4,9 @@ class supermarket:
         self.name=name
         self.location=location
         self.reyting=reyting
+    tovar={}
+    def add_basket(self, product, price):
+        supermarket.tovar[product]=price
 market = supermarket("Al_ofiyat","SHahriston","O'ta baland sifatli")
 
     
